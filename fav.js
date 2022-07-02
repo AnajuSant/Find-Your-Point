280ba86b1e7b52b6242173da16df40327f196ec0
Maria Damo <mdamodacosta@yahoo.com.br>
2022-07-01 22:49:11 -0300

    adicionado algumas animações

---
+++ fav.js
@@ -0,0 +1,13 @@
+const card_name = document.getElementById("name")
+const card_img = document.getElementById("card_img")
+// const evento = '{ "foto", "nome"}'
+
+// const evento_1_name = localStorage.setItem("evento_1_name", parse(card_name))
+const evento_1_img = localStorage.setItem("evento_1_img", card_img)
+
+console.log(card_img.currentSrc)
+console.log(card_name.innerText)
+
+
+// currentSrc
+// outerHTML
